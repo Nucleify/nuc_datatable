13aa52b5ee15012b2385c9b9dead2c31ee8af783
SzymCode <szymon.radomski@yahoo.com>
2026-01-30 16:45:24 +0100

    perf: async components

--- nuc_datatable.ts
+++ nuc_datatable.ts
@@ -1,14 +1,18 @@
 import type { App } from 'vue'
-
-import {
-  NucEntityDataTable,
-  NucEntityDataTableCard,
-  NucEntityDataTableSkeleton,
-} from '.'
+import { defineAsyncComponent, hydrateOnVisible } from 'vue'
 
 export function registerNucDataTable(app: App<Element>): void {
   app
-    .component('nuc-entity-datatable', NucEntityDataTable)
-    .component('nuc-entity-datatable-card', NucEntityDataTableCard)
-    .component('nuc-entity-datatable-skeleton', NucEntityDataTableSkeleton)
+    .component('nuc-entity-datatable', defineAsyncComponent({
+      loader: () => import('./atomic/templates/entity-datatable/index.vue'),
+      hydrate: hydrateOnVisible({ rootMargin: '500px' }),
+    }))
+    .component('nuc-entity-datatable-card', defineAsyncComponent({
+      loader: () => import('./atomic/templates/entity-datatable-card/index.vue'),
+      hydrate: hydrateOnVisible({ rootMargin: '500px' }),
+    }))
+    .component('nuc-entity-datatable-skeleton', defineAsyncComponent({
+      loader: () => import('./atomic/templates/entity-datatable-skeleton/index.vue'),
+      hydrate: hydrateOnVisible({ rootMargin: '500px' }),
+    }))
 }
